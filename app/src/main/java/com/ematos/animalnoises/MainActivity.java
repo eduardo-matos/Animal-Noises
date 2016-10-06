9826ba08eb5e3e70250c96875be3f3da62138e20
Eduardo Matos <eduardo.matos.silva@gmail.com>
2016-10-06 08:24:25 -0300

    make some noise

--- app/src/main/java/com/ematos/animalnoises/MainActivity.java
+++ app/src/main/java/com/ematos/animalnoises/MainActivity.java
@@ -1,13 +1,27 @@
 package com.ematos.animalnoises;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    protected MediaPlayer mplayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void makeNoise(View view) {
+        String animal = view.getTag().toString();
+        int resource_id = getResources().getIdentifier(animal, "raw", getPackageName());
+
+        if(mplayer != null)
+            mplayer.stop();
+
+        mplayer = MediaPlayer.create(this, resource_id);
+        mplayer.start();
     }
 }
